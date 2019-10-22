2fa986bbcbfaae724de1629adc60c7f8941a7e68
unknown <fdsa4321jkl@yahoo.com>
2019-10-22 16:50:00 -0700

    added more pages

--- routes/profile.js
+++ routes/profile.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
-router.get("/:platform/:gamertag", async (req, res) => {
+router.get("/:game/:platform/:gamertag", async (req, res) => {
   try {
     const headers = {
       "TRN-Api-Key": process.env.TRACKER_API_KEY
     };
-    const { platform, gamertag } = req.params;
+    const { game, platform, gamertag } = req.params;
     const response = await fetch(
-      `${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`,
+      `${process.env.TRACKER_API_URL}/${game}/standard/profile/${platform}/${gamertag}`,
       {
         headers: headers
       }
@@ -26,7 +26,7 @@ router.get("/:platform/:gamertag", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({
-      message: "Server Error"
+      message: "Server Errorasdfasdfasdf"
     });
   }
 });
